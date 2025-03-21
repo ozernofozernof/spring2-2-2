0656d1bddd3511296ee1eaa12ccf2608d671cc28
Pavel Kuznetsov <ozernof@gmail.com>
2025-03-21 11:57:22 +0300

    2 com

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.service.CarService;
+import web.service.CarServiceImpl;
 import web.model.Car;
 
 import java.util.List;
@@ -13,10 +13,10 @@ import java.util.List;
 @Controller
 public class CarController {
 
-    private final CarService carService;
+    private final CarServiceImpl carService;
 
     @Autowired
-    public CarController(CarService carService) {
+    public CarController(CarServiceImpl carService) {
         this.carService = carService;
     }
 
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,27 +1,8 @@
 package web.service;
 
-
-import org.springframework.stereotype.Service;
 import web.model.Car;
-
-import java.util.Arrays;
 import java.util.List;
 
-@Service
-public class CarService {
-
-    private final List<Car> cars = Arrays.asList(
-            new Car("Model S", "Tesla", 2020),
-            new Car("Mustang", "Ford", 2019),
-            new Car("Civic", "Honda", 2018),
-            new Car("Camry", "Toyota", 2021),
-            new Car("A4", "Audi", 2022)
-    );
-
-    public List<Car> getCars(Integer count) {
-        if (count == null || count >= cars.size()) {
-            return cars;
-        }
-        return cars.subList(0, count);
-    }
+public interface CarService {
+    List<Car> getCars(Integer count);
 }
